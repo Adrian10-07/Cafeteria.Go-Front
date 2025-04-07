0cbe51f0e1ea72c3dc860239bb7026ede9205ccb
diedgo05 <233358@ids.upchiapas.edu.mx>
2025-04-07 02:45:02 -0600

    logout

--- Cafeteria/src/app/shared/navbar/navbar.component.ts
+++ Cafeteria/src/app/shared/navbar/navbar.component.ts
@@ -1,18 +1,38 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import Swal from 'sweetalert2';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
-  constructor(){}
+  constructor(private router: Router) {}
 
-  isMenuOpen = false; // Controla la visibilidad del menú hamburguesa
 
-  toggleMenu() {
-    this.isMenuOpen = !this.isMenuOpen;
-    console.log('Menú toggled:', this.isMenuOpen); // Para depurar
+  logout() {
+    Swal.fire({
+      title: '¿Cerrar sesión?',
+      text: 'Se cerrará tu sesión actual',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#d33',
+      cancelButtonColor: '#3085d6',
+      confirmButtonText: 'Sí, cerrar sesión',
+      cancelButtonText: 'Cancelar'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        localStorage.clear();
+        this.router.navigate(['']);
+        Swal.fire(
+          'Sesión cerrada',
+          'Has salido correctamente.',
+          'success'
+        );
+      }
+    });
   }
+
 
   goBack() {
     window.history.back(); // Retrocede en la navegación
